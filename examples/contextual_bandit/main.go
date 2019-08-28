560a93183760aa81f5d9bb8e433e85ec7d5f01d2
orktes <jaakko.lukkari@gmail.com>
2019-08-28 21:20:47 +0300

    use weighted random instead of maxArg

--- examples/contextual_bandit/main.go
+++ examples/contextual_bandit/main.go
@@ -34,6 +34,24 @@ func argMax(vals []float32) int {
 	return maxIndx
 }
 
+func weightedRandom(vals []float32) int {
+	sum := float32(0)
+	for _, val := range vals {
+		sum += val
+	}
+
+	r := rand.Float32() * sum
+
+	for i, val := range vals {
+		r -= val
+		if r <= 0 {
+			return i
+		}
+	}
+
+	return 0
+}
+
 func main() {
 	vw, err := wabbit.New("--cb_explore 4 --cover 3")
 	if err != nil {
@@ -57,7 +75,7 @@ func main() {
 		vw.Predict(predictExample)
 
 		scores := predictExample.GetActionScores()
-		selected := argMax(scores)
+		selected := weightedRandom(scores)
 
 		cost := 0.0
 		if selected != indexOf(contextValue, contextValues) {
